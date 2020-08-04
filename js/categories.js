48c0d1a1b228ed3829460748e404503d793b65f0
Michał Ziemski <mz@e-adm.pl>
2020-08-04 11:34:01 +0200

    spis treści

--- js/categories.js
+++ js/categories.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     var posts = document.getElementById("posts");
     function e(t, cls, att, cldf) {
         var el = document.createElement(t);
@@ -17,60 +17,45 @@
         return document.createTextNode(txt);
     }
     if (posts) {
-        postsPromise().then(function(postsData) {
+        postsPromise().then(function (postsData) {
             posts.appendChild(
-                e("div", "container", {}, function() {
-                    var cat0 = []
-                    for (var i = 0; i < postsData.length; i++) {
-                        cat0.push(
-                            e("div", "row", {}, function() {
-                                var cat1 = [
-                                    e("div", "col-12", {}, function() {
-                                        return [
-                                            e("h4", "", {}, function() {
-                                                return [
-                                                    t(postsData[i][0])
-                                                ]
-                                            })
-                                        ]
-                                    })
-                                ]
-                                for (var j = 0; j < postsData[i][1].length; j++) {
-                                    cat1.push(
-                                        e("div", "col-12 col-md-6 col-xl-4", {}, function() {
+                e("div", "container", {}, function () {
+                    return [e("div", "row", {}, function () {
+                        var cat0 = []
+                        for (var i = 0; i < postsData.length; i++) {
+                            cat0.push(
+                                e("div", "col-12 col-md-6", {}, function () {
+                                    return [
+                                        e("h4", "", {}, function () {
                                             return [
-                                                e("h5", "", {}, function() {
-                                                    return [
-                                                        t(postsData[i][1][j][0])
-                                                    ]
-                                                }),
-                                                e("ul", "", {}, function() {
-                                                    var cat2 = [];
-                                                    for (var k = 0; k < postsData[i][1][j][1].length; k++) {
-                                                        cat2.push(
-                                                            e("li", "", {}, function() {
+                                                t(postsData[i][0])
+                                            ]
+                                        }),
+                                        e("ul", "", {}, function () {
+                                            var cat1 = [];
+                                            for (var j = 0; j < postsData[i][1].length; j++) {
+                                                cat1.push(
+                                                    e("li", "", {}, function () {
+                                                        return [
+                                                            e("a", "", { "href": baseUrl + postsData[i][1][j][1] }, function () {
                                                                 return [
-                                                                    e("a", "", { "href": baseUrl + postsData[i][1][j][1][k][1] }, function() {
-                                                                        return [
-                                                                            t(postsData[i][1][j][1][k][0])
-                                                                        ]
-                                                                    })
+                                                                    t(postsData[i][1][j][0])
                                                                 ]
                                                             })
-                                                        )
-                                                    }
-                                                    return cat2;
-                                                })
-                                            ]
+                                                        ]
+                                                    })
+                                                )
+                                            }
+                                            return cat1;
                                         })
-                                    )
-                                }
-                                return cat1;
-                            })    
-                        )
-                    }
-                    return cat0;
-                }));
-        })
+                                    ]
+                                })
+                            )
+                        }
+                        return cat0
+                    })]
+                })
+            );
+        });
     }
 })()
